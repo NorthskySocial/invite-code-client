1413e9e1c19bcf4d0bdb934dc71f6e561024d476
Andrés Ignacio Torres <dev@aitorres.com>
2025-09-11 23:03:07 -0700

    style: cargo fmt

--- src/styles.rs
+++ src/styles.rs
@@ -81,7 +81,11 @@ pub fn render_input(
     ui.add_space(WIDGET_SPACING_BASE);
 }
 
-pub fn render_base_input(text: &mut String, is_password: bool, _use_frame: bool) -> egui::TextEdit<'_> {
+pub fn render_base_input(
+    text: &mut String,
+    is_password: bool,
+    _use_frame: bool,
+) -> egui::TextEdit<'_> {
     egui::TextEdit::singleline(text)
         .password(is_password)
         .desired_width(INPUT_WIDTH)
